943ab06669a722050ccb5f0c9415e6b68831f025
Akshaykukku <87674854+Akshaykukku@users.noreply.github.com>
2023-09-06 03:45:51 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,12 +1,12 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", "0112234"))
-    API_HASH = getenv("API_HASH", "abcdefg")
-    BOT_TOKEN = getenv("BOT_TOKEN", "1234567891:AdDfgFRFVVfDEhdhyjjvjjftSEW")
-    FSUB = getenv("FSUB", "SDBotz")
-    CHID = int(getenv("CHID", "-1000112234"))
-    SUDO = list(map(int, getenv("SUDO").split()))
-    MONGO_URI = getenv("MONGO_URI", "")
+    API_ID = int(getenv("API_ID", "22440762"))
+    API_HASH = getenv("API_HASH", "f18c585387a50ef82cf4eab05be6dd59")
+    BOT_TOKEN = getenv("BOT_TOKEN", "5958352109:AAEW4bqGZaZiIo-IoBpiAWh9rV5iWemjM6U")
+    FSUB = getenv("FSUB", "Spyromovies")
+    CHID = int(getenv("CHID", "-1001647019088"))
+    SUDO = list(map(int, getenv("1361111830").split()))
+    MONGO_URI = getenv("MONGO_URI", "d1731c0756deb8ad0651f847d11ab4e7")
     
 cfg = Config()
